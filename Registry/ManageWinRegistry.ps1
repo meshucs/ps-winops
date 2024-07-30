f79977fbd0cf35f8469cb4dddc3ecd44d99ccbb9
Meshu <code@labs.meshu.us>
2024-07-30 13:06:50 -0400

    Create ManageWinRegistry.ps1

---
+++ Registry/ManageWinRegistry.ps1
@@ -0,0 +1,26 @@
+﻿function ManageRegistry([string]$registryPath, [string]$registryKey, [string]$registryValue, [string]$action = 'install') {
+
+    $registryFqn = "$registryPath\$registryKey"
+
+    if ($action.ToLower() -eq 'install') {
+
+        $_registry = (Get-ItemProperty $registryPath).PSObject.Properties.Name -Contains $registryKey
+
+
+        if (($_registry -eq $null) -or ($_registry.Length -eq 0)) {
+            ManageConsoleOutput -message "Deploying registry: $registryFqn."
+            New-ItemProperty $registryPath -Name $registryKey -Value $registryValue
+        }
+        else {
+            ManageConsoleOutput -message "Registry already deployed: $registryFqn."
+        }
+    
+        Get-ItemProperty -Path $registryPath -Name $registryKey
+    }
+
+    if ($action.ToLower() -eq 'delete') { 
+        Remove-ItemProperty $registryPath -Name $registryKey
+    
+    }
+
+}
